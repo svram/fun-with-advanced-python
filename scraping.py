419af57818e451f86a8cec9b6d531e4fd352da96
Vikram <vikram.denizen@gmail.com>
2017-03-17 13:42:37 +0800

    create scraping.py

---
+++ scraping.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+'''
+	Web scraping demo. 
+
+	Note: make sure to create a virtualenv and run:
+		$ pip install -r requirements.txt
+	This demo uses the requests and BeautifulSoup libraries
+
+	Usage: 
+	
+	© 2017 Vikram Bahl
+'''
+
+import requests
+from bs4 import BeautifulSoup
