1f7110dae007f3b029607ce9bd59a847b208d42b
Vikram <vikram.denizen@gmail.com>
2017-03-29 22:48:26 +0800

    update wordCloud.py

---
+++ useful-scripts/wordCloud.py
@@ -0,0 +1,9 @@
+'''
+	A word cloud is visual representation of the frequency of words in a corpus 
+	with frequency of the word directly proportional to its font size in the representation
+
+	Now the first step in creating the word cloud is to clean the corpus removing
+	unnecessary punctuation. The next step is the hard part.
+
+	Decision time. 
+'''
