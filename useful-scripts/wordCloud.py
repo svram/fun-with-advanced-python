922a9b900ed39ea7a3a22a72c7758a275465315f
Vikram <vikram.denizen@gmail.com>
2017-03-30 09:46:48 +0800

    discuss tradeoffs of wordCloud

--- useful-scripts/wordCloud.py
+++ useful-scripts/wordCloud.py
@@ -5,5 +5,13 @@
 	Now the first step in creating the word cloud is to clean the corpus removing
 	unnecessary punctuation. The next step is the hard part.
 
-	Decision time. 
+	Does the apostrophe count as punctuation? What if someone uses the word "How's" instead of "How is"
+	What about the underscore and the "-"?
+	Would you treat uppercase, capitalised and lowercase differently?
+	How about personal nouns?
+	What about a word which is context dependent? Like "Hello Jack" and "This is a jack hammer"
+
+	Dependig upon the situation, you would make a decision on the tradeoffs.
+	Here are the assumptions we are going with:
+		- 
 '''
