6480f2a3270cfdb38d390111c1c1b64ec2012aaf
Vikram <vikram.denizen@gmail.com>
2017-04-04 16:34:21 +0800

    add system info module

---
+++ useful-scripts/system_info.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+#!/usr/bin/env python 
+ 
+''' 
+ 		Useful function stubs to retirive information about your OS, hardware, network connection etc
+ 
+		© 2017 Vikram Bahl 
+'''
