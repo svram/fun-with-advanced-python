d7ad493044fff55f2b54b6f616846e934d26ed37
Vikram <vikram.denizen@gmail.com>
2017-04-10 10:46:52 +0800

    add platform information

--- useful-scripts/system_info.py
+++ useful-scripts/system_info.py
@@ -2,7 +2,10 @@
 #!/usr/bin/env python 
  
 ''' 
- 		Useful function stubs to retirive information about your OS, hardware, network connection etc
+ 		Useful function stubs to retrieve information about your OS, hardware, network connection.
+ 		The commands are pretty straightforward and function calls are self-explanatory.
+ 		Check out the documentation for the os, sys and platfirm modules:
+ 			https://docs.python.org
  
 		© 2017 Vikram Bahl 
 '''
@@ -10,7 +13,10 @@
 import os, sys, platform
 
 def get_machine_info():
-	print platform.machine()
+	
+	print "MACHINE TYPE: {}".format(platform.machine())
+	print "PLATFORM: {}".format(platform.platform(aliased=True, terse=False))
+	print "PROCESSOR: {}".format(platform.processor())
 
 if __name__ == '__main__':
 	get_machine_info()
